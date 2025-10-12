ab4cfb590160e4b9d02db9f7163cf411e6425774
BhargavGarge <bhargaavvv@gmail.com>
2025-10-12 15:32:28 +0200

    Implented name and welcome screen

--- src/navigation/StackNavigator.js
+++ src/navigation/StackNavigator.js
@@ -9,6 +9,26 @@ import ProfileScreen from '../screens/ProfileScreen';
 
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { NavigationContainer } from '@react-navigation/native';
+import LoginScreen from '../screens/LoginScreen';
+import BasicInfo from '../screens/BasicInfo';
+import NameScreen from '../screens/NameScreen';
+import EmailScreen from '../screens/EmailScreen';
+import OtpScreen from '../screens/OtpScreen';
+import PasswordScreen from '../screens/PasswordScreen';
+import DOBScreen from '../screens/DOBScreen';
+import LocationScreen from '../screens/LocationScreen';
+import GenderScreen from '../screens/GenderScreen';
+import TypeScreen from '../screens/TypeScreen';
+import DatingTypeScreen from '../screens/DatingTypeScreen';
+import LookingForScreen from '../screens/LookingForScreen';
+import HomeTownScreeen from '../screens/HomeTownScreeen';
+import WorkplaceScreen from '../screens/WorkplaceScreen';
+import JobTitleScreen from '../screens/JobTitleScreen';
+import PhotosScreen from '../screens/PhotosScreen';
+import PromptScreen from '../screens/PromptScreen';
+import ShowPromptScreen from '../screens/ShowPromptScreen';
+import WritePromptScren from '../screens/WritePromptScren';
+import PreFinalScreen from '../screens/PreFinalScreen';
 const StackNavigator = () => {
   const Stack = createNativeStackNavigator();
   const Tab = createBottomTabNavigator();
@@ -120,6 +140,115 @@ const StackNavigator = () => {
     );
   }
 
+  const AuthStack = () => {
+    return (
+      <Stack.Navigator>
+        {/* <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        /> */}
+        <Stack.Screen
+          name="Basic"
+          component={BasicInfo}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Name"
+          component={NameScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Email"
+          component={EmailScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Otp"
+          component={OtpScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Password"
+          component={PasswordScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Birth"
+          component={DOBScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Location"
+          component={LocationScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Gender"
+          component={GenderScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Type"
+          component={TypeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Dating"
+          component={DatingTypeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="LookingFor"
+          component={LookingForScreen}
+          options={{ headerShown: false }}
+        />
+
+        <Stack.Screen
+          name="Hometown"
+          component={HomeTownScreeen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Workplace"
+          component={WorkplaceScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="JobTitle"
+          component={JobTitleScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Photos"
+          component={PhotosScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Prompts"
+          component={PromptScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="ShowPrompts"
+          component={ShowPromptScreen}
+          options={{ headerShown: false }}
+        />
+
+        <Stack.Screen
+          name="WritePrompt"
+          component={WritePromptScren}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="PreFinal"
+          component={PreFinalScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    );
+  };
+
   function MainStack() {
     return (
       <Stack.Navigator>
@@ -133,7 +262,8 @@ const StackNavigator = () => {
   }
   return (
     <NavigationContainer>
-      <MainStack />
+      {/* <MainStack /> */}
+      <AuthStack />
     </NavigationContainer>
   );
 };
---
+++ src/screens/DatingTypeScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const DatingTypeScreen = () => {
+  return (
+    <View>
+      <Text>DatingTypeScreen</Text>
+    </View>
+  );
+};
+
+export default DatingTypeScreen;
+
+const styles = StyleSheet.create({});
