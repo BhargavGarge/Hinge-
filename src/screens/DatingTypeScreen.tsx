c08e44d9652fe7401c0e6172a030c2c93700bfe2
BhargavGarge <bhargaavvv@gmail.com>
2025-10-15 11:40:09 +0200

    implemented user info screens

--- src/screens/DatingTypeScreen.tsx
+++ src/screens/DatingTypeScreen.tsx
@@ -1,14 +1,180 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  Image,
+  Pressable,
+  TouchableOpacity,
+} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome6 from 'react-native-vector-icons/fontawesome';
 
-const DatingTypeScreen = () => {
+import { useNavigation } from '@react-navigation/native';
+import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
+
+type RootStackParamList = {
+  Dating: undefined;
+  LookingFor: undefined;
+};
+const DatingType = () => {
+  const [datingPreferences, setDatingPreferences] = useState<string[]>([]);
+  const navigation =
+    useNavigation<NativeStackNavigationProp<RootStackParamList, 'Dating'>>();
+  interface ChooseOption {
+    (option: string): void;
+  }
+
+  const chooseOption: ChooseOption = option => {
+    if (datingPreferences.includes(option)) {
+      setDatingPreferences(
+        datingPreferences.filter(
+          (selectedOption: string) => selectedOption !== option,
+        ),
+      );
+    } else {
+      setDatingPreferences([...datingPreferences, option]);
+    }
+  };
+
+  const handleNext = () => {
+    navigation.navigate('LookingFor');
+  };
   return (
-    <View>
-      <Text>DatingTypeScreen</Text>
-    </View>
+    <SafeAreaView
+      style={{
+        paddingTop: Platform.OS === 'android' ? 35 : 0,
+        flex: 1,
+        backgroundColor: 'white',
+      }}
+    >
+      <View style={{ marginTop: 80, marginHorizontal: 20 }}>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <View
+            style={{
+              width: 44,
+              height: 44,
+              borderRadius: 22,
+              borderWidth: 2,
+              borderColor: 'black',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Ionicons name="gender-male" size={23} color="black" />
+          </View>
+          <Image
+            style={{ width: 100, height: 40 }}
+            source={{
+              uri: 'https://cdn-icons-png.flaticon.com/128/10613/10613685.png',
+            }}
+          />
+        </View>
+
+        <Text
+          style={{
+            fontSize: 25,
+            fontWeight: 'bold',
+            fontFamily: 'GeezaPro-Bold',
+            marginTop: 15,
+          }}
+        >
+          Who do you want to date?
+        </Text>
+
+        <Text style={{ fontSize: 15, marginTop: 20, color: 'gray' }}>
+          Select all people you're open to meeting
+        </Text>
+
+        <View style={{ marginTop: 30, flexDirection: 'column', gap: 12 }}>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Text style={{ fontSize: 15, fontWeight: '500' }}>Men</Text>
+            <Pressable onPress={() => chooseOption('Men')}>
+              <FontAwesome6
+                name="circle"
+                size={26}
+                color={
+                  datingPreferences.includes('Men') ? '#581845' : '#F0F0F0'
+                }
+              />
+            </Pressable>
+          </View>
+
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Text style={{ fontSize: 15, fontWeight: '500' }}>Women</Text>
+            <Pressable onPress={() => chooseOption('Women')}>
+              <FontAwesome6
+                name="circle"
+                size={26}
+                color={
+                  datingPreferences.includes('Women') ? '#581845' : '#F0F0F0'
+                }
+              />
+            </Pressable>
+          </View>
+
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Text style={{ fontSize: 15, fontWeight: '500' }}>Everyone</Text>
+            <Pressable onPress={() => chooseOption('Everyone')}>
+              <FontAwesome6
+                name="circle"
+                size={26}
+                color={
+                  datingPreferences.includes('Everyone') ? '#581845' : '#F0F0F0'
+                }
+              />
+            </Pressable>
+          </View>
+
+          <View
+            style={{
+              marginTop: 30,
+              flexDirection: 'row',
+              alignItems: 'center',
+              gap: 8,
+            }}
+          >
+            <Ionicons name="checkbox-outline" size={25} color="#900C3F" />
+            <Text style={{ fontSize: 15 }}>Visible on profile</Text>
+          </View>
+        </View>
+
+        <TouchableOpacity
+          onPress={handleNext}
+          activeOpacity={0.8}
+          style={{ marginTop: 30, marginLeft: 'auto' }}
+        >
+          <Ionicons
+            name="chevron-forward-circle-outline"
+            size={45}
+            color="#581845"
+          />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
 
-export default DatingTypeScreen;
+export default DatingType;
 
 const styles = StyleSheet.create({});
--- src/screens/PhotosScreen.tsx
+++ src/screens/PhotosScreen.tsx
@@ -1,14 +1,216 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  TouchableOpacity,
+  Image,
+  Pressable,
+  TextInput,
+  Button,
+} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import Ionicons from 'react-native-vector-icons/ionicons';
+import EvilIcons from '@react-native-vector-icons/evil-icons';
 
-const PhotosScreen = () => {
+import { useNavigation } from '@react-navigation/native';
+import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
+
+type RootStackParamList = {
+  Prompts: undefined;
+  Photos: undefined;
+};
+const PhotoScreen = () => {
+  const [imageUrls, setImageUrls] = useState(['', '', '', '', '', '']);
+  const [imageUrl, setImageUrl] = useState('');
+  const navigation =
+    useNavigation<NativeStackNavigationProp<RootStackParamList, 'Photos'>>();
+  const handleAddImage = () => {
+    const index = imageUrls?.findIndex(url => url === '');
+    if (index !== -1) {
+      const updatedUrls = [...imageUrls];
+      updatedUrls[index] = imageUrl;
+      setImageUrls(updatedUrls);
+      setImageUrl('');
+    }
+  };
+
+  const handleNext = () => {
+    navigation.navigate('Prompts');
+  };
+  console.log('images', imageUrls);
   return (
-    <View>
-      <Text>PhotosScreen</Text>
-    </View>
+    <SafeAreaView
+      style={{
+        paddingTop: Platform.OS === 'android' ? 35 : 0,
+        flex: 1,
+        backgroundColor: 'white',
+      }}
+    >
+      <View style={{ marginTop: 80, marginHorizontal: 20 }}>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <View
+            style={{
+              width: 44,
+              height: 44,
+              borderRadius: 22,
+              borderWidth: 2,
+              borderColor: 'black',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Ionicons name="camera-outline" size={23} color="black" />
+          </View>
+          <Image
+            style={{ width: 100, height: 40 }}
+            source={{
+              uri: 'https://cdn-icons-png.flaticon.com/128/10613/10613685.png',
+            }}
+          />
+        </View>
+
+        <Text
+          style={{
+            fontSize: 25,
+            fontWeight: 'bold',
+            fontFamily: 'GeezaPro-Bold',
+            marginTop: 15,
+          }}
+        >
+          Pick your photos and videos
+        </Text>
+
+        <View style={{ marginTop: 20 }}>
+          <View style={{ flexDirection: 'row', alignItems: 'center', gap: 20 }}>
+            {imageUrls?.slice(0, 3).map((url, index) => (
+              <Pressable
+                style={{
+                  borderColor: '#581845',
+                  borderWidth: url ? 0 : 2,
+                  flex: 1,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  borderStyle: 'dashed',
+                  borderRadius: 10,
+                  height: 100,
+                }}
+                key={index}
+              >
+                {url ? (
+                  <Image
+                    source={{ uri: url }}
+                    style={{
+                      width: '100%',
+                      height: '100%',
+                      borderRadius: 10,
+                      resizeMode: 'cover',
+                    }}
+                  />
+                ) : (
+                  <EvilIcons name="image" size={22} color="black" />
+                )}
+              </Pressable>
+            ))}
+          </View>
+        </View>
+
+        <View style={{ marginTop: 20 }}>
+          <View style={{ flexDirection: 'row', alignItems: 'center', gap: 20 }}>
+            {imageUrls?.slice(3, 6).map((url, index) => (
+              <Pressable
+                style={{
+                  borderColor: '#581845',
+                  borderWidth: url ? 0 : 2,
+                  flex: 1,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  borderStyle: 'dashed',
+                  borderRadius: 10,
+                  height: 100,
+                }}
+                key={index}
+              >
+                {url ? (
+                  <Image
+                    source={{ uri: url }}
+                    style={{
+                      width: '100%',
+                      height: '100%',
+                      borderRadius: 10,
+                      resizeMode: 'cover',
+                    }}
+                  />
+                ) : (
+                  <EvilIcons name="image" size={22} color="black" />
+                )}
+              </Pressable>
+            ))}
+          </View>
+
+          <View style={{ marginVertical: 10 }}>
+            <Text style={{ color: 'gray', fontSize: 15 }}>Drag to reorder</Text>
+
+            <Text
+              style={{
+                marginTop: 4,
+                color: '#581845',
+                fontWeight: '500',
+                fontSize: 15,
+              }}
+            >
+              Add four to six photos
+            </Text>
+          </View>
+
+          <View style={{ marginTop: 25 }}>
+            <Text>Add a picture of yourself</Text>
+
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                gap: 5,
+                borderRadius: 5,
+                marginTop: 10,
+                backgroundColor: '#DCDCDC',
+                paddingVertical: 6,
+              }}
+            >
+              <EvilIcons
+                name="image"
+                style={{ marginLeft: 8 }}
+                size={22}
+                color="black"
+              />
+              <TextInput
+                value={imageUrl}
+                onChangeText={text => setImageUrl(text)}
+                style={{ color: 'gray', marginVertical: 10, width: 300 }}
+                placeholder="Enter your image url"
+              />
+            </View>
+            <Button onPress={handleAddImage} title="Add Image" />
+          </View>
+        </View>
+
+        <TouchableOpacity
+          onPress={handleNext}
+          activeOpacity={0.8}
+          style={{ marginTop: 30, marginLeft: 'auto' }}
+        >
+          <Ionicons
+            name="chevron-forward-circle-outline"
+            size={45}
+            color="#581845"
+          />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
 
-export default PhotosScreen;
+export default PhotoScreen;
 
 const styles = StyleSheet.create({});
